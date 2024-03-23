e56c979976a542abadae725ca15fb5d8c78f1baa
CamialaRodriguez <137795930+CamialaRodriguez@users.noreply.github.com>
2024-03-23 08:28:41 -0500

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,49 @@
+function abrirPelicula(){
+    let pelicula = document.getElementById('modal-pelicula')
+    pelicula.classList.toggle('modal');
+}
+
+function abrirPelicula1(){
+    let pelicula = document.getElementById('modal-pelicula1')
+    pelicula.classList.toggle('modal');
+}
+
+function abrirPelicula2(){
+    let pelicula = document.getElementById('modal-pelicula2')
+    pelicula.classList.toggle('modal');
+}
+
+function abrirPelicula3(){
+    let pelicula = document.getElementById('modal-pelicula3')
+    pelicula.classList.toggle('modal');
+}
+
+function abrirPelicula4(){
+    let pelicula = document.getElementById('modal-pelicula4')
+    pelicula.classList.toggle('modal');
+}
+
+function abrirPelicula5(){
+    let pelicula = document.getElementById('modal-pelicula5')
+    pelicula.classList.toggle('modal');
+}
+
+function abrirPelicula6(){
+    let pelicula = document.getElementById('modal-pelicula6')
+    pelicula.classList.toggle('modal');
+}
+
+function abrirPelicula7(){
+    let pelicula = document.getElementById('modal-pelicula7')
+    pelicula.classList.toggle('modal');
+}
+
+
+
+
+function Panel(){
+    let panel = document.getElementById('panel-lateral');
+    panel.classList.toggle('active');
+}
+
+
